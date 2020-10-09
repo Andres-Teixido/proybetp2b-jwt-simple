05fe8bf11bb8ee4a90d3cf5f14bd529664456f07
Andres Teixido Semirozun <andresteix@gmail.com>
2020-10-08 21:11:56 -0300

    up connection

--- data/connectionMongo.js
+++ data/connectionMongo.js
@@ -1,9 +1,9 @@
-// mongodb+srv://admin:betp2b@cluster0.7et2f.mongodb.net/sample_betp2b?retryWrites=true&w=majority
+// mongodb+srv://admin:betp2b@cluster0.lhrma.mongodb.net/sample_betp2b?retryWrites=true&w=majority
 const mongoClient = require('mongodb').MongoClient;
 const chalk = require('chalk');
 
 // TODO: cambiar por variables de entorno
-const uriMongo = 'mongodb+srv://admin:betp2b@cluster0.7et2f.mongodb.net/sample_betp2b?retryWrites=true&w=majority';
+const uriMongo = 'mongodb+srv://admin:betp2b@cluster0.lhrma.mongodb.net/sample_betp2b?retryWrites=true&w=majority';
 
 const client = new mongoClient(uriMongo, {useUnifiedTopology: true, useNewUrlParser: true });
 
